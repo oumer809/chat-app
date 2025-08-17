684f3bc99b679425388634ee106e24727fcb48f5
oumer809 <oumersherif809@gmail.com>
2025-08-17 15:08:05 +0000

    rtr

--- fbchatapp/src/components/Chat.jsx
+++ fbchatapp/src/components/Chat.jsx
@@ -4,13 +4,18 @@ import { auth, db } from '../firebase'
 
 const Chat = ({room}) => {
     const [message, setMessage] =useState('')
+    const [messages, setMessages] =useState([])
 
     const messageRef = collection(db, 'messages');
 
     useEffect(()=>{
        const queryMessages = query(messageRef, where('room','==', room));
        onSnapshot(queryMessages, (snapshot)=>{
-         console.log('')
+        let messages = [];
+        snapshot.forEach((doc)=>{
+            messages.push({ ...doc.data, id:doc.id });
+        });
+        setMessages(messages )
        });
     },[])
 
@@ -28,6 +33,13 @@ const Chat = ({room}) => {
     }
   return (
     <div className='flex items-center justify-center h-screen '>
+       <div>
+         {messages.map((message, index)=> (
+            <ul key={index}>
+                <li>{message.text}</li>
+            </ul>
+         ))}
+       </div>
         <form onSubmit={handleSubmit} className='border-2 border-gray-600 bg-gray-300 p-6 '>
             <input type="text" placeholder='type your message' className='px-4 py-1 'value={message} onChange={(e)=> setMessage(e.target.value)}/>
             <button type='submit' className='px-4 py-2 bg-blue-500 rounded-md'>Send</button>
